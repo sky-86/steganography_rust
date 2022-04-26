31c796b38e3930d57d0a445f21ac2c2cd90a2eed
sky-86 <sky86one@gmail.com>
2022-04-25 23:05:12 -0500

    remade the get_line_of_bytes function, 13 lines to 2

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,37 @@ use std::convert::TryFrom;
 use std::fs;
 use std::io::Read;
 
-struct Image {
-    magic_num: String,
-    comment: String,
-    dim: String,
-    range: String,
-    pixels: String,
-}
-
 // takes the img path, returns the bytes of the img
 fn read_bytes(filename: String) -> Vec<u8> {
     let mut contents = Vec::new();
     let mut file = fs::File::open(&filename).expect("Error opening file");
     file.read_to_end(&mut contents).expect("Unable to read");
 
-    contents.reverse();
     contents
 }
 
 // returns group of bytes based on <eol>
+fn get_vec_of_img(bytes: &Vec<u8>) -> Vec<&[u8]> {
+    let lines: Vec<_> = bytes.split_inclusive(|x| *x == 10).collect();
+    lines
+}
+
+/*
 fn get_line_of_bytes(bytes: &mut Vec<u8>) -> Vec<u8> {
     let mut line: Vec<u8> = Vec::new();
     while !bytes.ends_with(&[10]) {
         match bytes.pop() {
             Some(x) => line.push(x),
-
             None => break,
         }
     }
     match bytes.pop() {
         Some(x) => line.push(x),
-
         None => (),
     }
     line
 }
+*/
 
 fn string_to_binary(s: &String) -> String {
     let mut binary = String::new();
@@ -61,14 +57,11 @@ fn create_msg_header(length: &u16, depth: &u16) -> String {
 fn encode_msg(pixels: &mut Vec<u8>, header: &String, msg: &String) {}
 
 fn main() {
-    let mut img_bytes = read_bytes(String::from("test_p6.ppm"));
+    let img_bytes = read_bytes(String::from("test_p6.ppm"));
 
-    let mut magic_num = get_line_of_bytes(&mut img_bytes);
-    let mut comment = get_line_of_bytes(&mut img_bytes);
-    let mut dimensions = get_line_of_bytes(&mut img_bytes);
-    let mut color_range = get_line_of_bytes(&mut img_bytes);
-
-    let mut pixels = get_line_of_bytes(&mut img_bytes);
+    let mut img_vec = get_vec_of_img(&img_bytes);
+    println!("{:?}", img_vec);
+    let pixels = img_vec.pop().unwrap();
 
     let msg = string_to_binary(&String::from("Hello World"));
     let msg_header = create_msg_header(&u16::try_from(msg.len()).unwrap(), &32);
